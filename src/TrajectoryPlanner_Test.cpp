72d24ccdf1e81276c0a719fefe126ac5484c0a32
Dan Bergeland <dan.bergeland@workiva.com>
2017-08-12 12:33:13 -0500

    Added test framework and empty test

---
+++ src/TrajectoryPlanner_Test.cpp
@@ -0,0 +1,16 @@
+//
+//  TrajectoryPlanner_Test.cpp
+//  
+//
+//  Created by Dan Bergeland on 8/12/17.
+//
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <stdio.h>
+
+TEST_CASE("First Test", "TrajectoryPlanner"){
+    REQUIRE(false);
+}
